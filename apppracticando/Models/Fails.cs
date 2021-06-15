c43bdc0c58a465091d6915f6a35694a43a60d0b8
ROBINSON MORENO <robinson.moreno@upch.pe>
2021-06-15 17:53:54 -0500

    creando modelo y pagina inicial

---
+++ apppracticando/Models/Fails.cs
@@ -0,0 +1,22 @@
+
+
+using System;
+namespace apppracticando.Models
+{
+    public class Fails
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Foto { get; set; }
+
+
+        public DateTime Fecha { get; set; }
+
+
+        public string Usuario { get; set; }
+
+        
+    }
+}
