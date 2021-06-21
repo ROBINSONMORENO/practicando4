f36041b4e6bfb825a049ca14009cb899d38e61d1
ROBINSONMORENO <80597702+ROBINSONMORENO@users.noreply.github.com>
2021-06-21 14:28:01 -0500

    Update apppracticandoContext.cs

--- apppracticando/Models/apppracticandoContext.cs
+++ apppracticando/Models/apppracticandoContext.cs
@@ -1,15 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
+using apppracticando.Models;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace apppracticando.Models
+namespace apppracticando.Data
 {
-    public class apppracticandoContext : DbContext
-    {
-     
-       public DbSet<Fails> Failss{ get; set;} 
-       public  apppracticandoContext(DbContextOptions dco) : base(dco) {
-
-        }  
-
-                              
+    public class ApppracticandoDbContext : IdentityDbContext 
+    {       
+        public DbSet<Fail> Fails { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        public ApppracticandoDbContext (DbContextOptions dco) : base(dco)
+        {
+        }
+  
     }
 }
